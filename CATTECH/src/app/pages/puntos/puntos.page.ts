010f3a5744ebbcf5036d55ff6561164a0bcebf93
German-Cortes <ger.cortes@duocuc.cl>
2024-09-15 08:48:01 -0300

    actualizPuntos

--- CATTECH/src/app/pages/puntos/puntos.page.ts
+++ CATTECH/src/app/pages/puntos/puntos.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./puntos.page.scss'],
 })
 export class PuntosPage implements OnInit {
+  
+  //  defines la propiedad providers como un array de objetos
+  providers = [
+    { name: 'Reciladora1', distance: '0 km', address: 'viña' },
+    { name: 'Punto de reparacion', distance: '0.2 km', address: 'viña' },
+    { name: 'Recicladora3', distance: '0.4 km', address: 'viña' },
+    
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    //  obtener los proveedores desde una API
   }
-
 }
