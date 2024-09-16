5d284a9f9642c134b6ef93f38b03f5ac6e25a4f0
German-Cortes <ger.cortes@duocuc.cl>
2024-09-16 07:23:10 -0300

    ajustesvarios

--- CATTECH/src/app/pages/contacto/contacto.page.ts
+++ CATTECH/src/app/pages/contacto/contacto.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+ //  importar Clipboard
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
 })
-export class ContactoPage implements OnInit {
+export class ContactoPage {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  // Método para copiar al portapapeles
+ // async copyToClipboard(text: string) {
+   // await Clipboard.write({
+     // string: text
+    //});
+    //console.log('Correo copiado al portapapeles:', text);
+    // Aquí puedes mostrar un mensaje de éxito con un Toast o Alert si deseas.
   }
-
-}
